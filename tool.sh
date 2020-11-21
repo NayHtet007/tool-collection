82532d50833ee26d0f56a690e2d956ff10f879ca
ypn007 <55645320+ypn007@users.noreply.github.com>
2020-11-21 16:33:38 +0000

    Create tool.sh

---
+++ tool.sh
@@ -0,0 +1,189 @@
+echo -e "\e[1;36m
+###########################################################
+\e[0m"
+ figlet  'Cyber Bullet' 
+echo -e "\e[1;36m
+###########################################################\e[0m"
+echo ""
+echo -e "\e[1;33m  
+Github.com=https://github.com/Nay-Htet007
+Facebook.com=https://www.facebook.com/nhjs51gr
+Instagram.com=https://www.instagram.com/nay_htet_51 
+Created by D4Rk-D8V1L
+\e[1;32m
+Supported By Yell Bhone Naing
+Respect Sir.Yell Bhone Naing
+\e[0m"
+echo -e "\e[1;36m
+___________________________________________________________\e[0m"
+echo "Wait for 10s.............."
+sleep 10s
+clear
+echo -e "\e[1;35m 
+¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\e[0m"
+USERNAME='Cyber Bullet'
+PASSWORD='Tool-C'
+read -p "ENTER YOUR User Name::;" name
+read -sp "ENTER YOUR PASSWORD::;" pass 
+if [[ $name == $USERNAME && $pass == $PASSWORD ]];then
+echo ""
+echo -e "\e[;35m¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\e[0m"
+echo -e "\e[1;32m Your login is successful.\e[0m" 
+sleep 3s
+clear
+ORANGE="\e[33m"
+GREEN="\e[92m"
+STOP="\e[0m"
+printf "${GREEN}"
+printf "=================================\n"
+printf "${ORANGE}"
+figlet -w 200 -f standard "Tool-C"
+printf "${GREEN}"
+printf "=================================\n"
+printf "${STOP}"
+echo ""
+echo -e "\e[1;32m ~~~###What you want to use? \e[0m"
+echo "
+>>(1)ninja_host
+>>(2)sql-scanner
+>>(3)T-Logo
+>>(4)Profileguard  
+>>(5)OSIF   
+>>(6)SocialPhish   (Phishing)
+>>(7)NayMyo-Tool
+>>(8)empas-limit
+>>(9)MoontonChecker
+>>(10)AdvPhishing  (Phishing)
+>>(007)Just-For-Fun(for fun)
+>>(00)exit" 
+read -p $'\e[1;32m ~~~###Enter your choice##>> \e[0m' asl
+case $asl in
+1)
+clear
+figlet 'ninja_host'
+pkg install git
+git clone https://github.com/T-Tools/ninja_host
+cd ninja_host
+bash ninja_host.sh
+;;
+2)
+clear
+figlet 'sql-scanner'
+pkg install figlet
+pkg install git
+pkg install php
+git clone https://github.com/T-Tools/sql-scanner
+cd sql-scanner
+php scan.php
+;;
+3)
+clear
+figlet 'T-Logo'
+pkg install figlet
+pkg install lolcat
+pkg install git
+pkg install toilet
+git clone https://github.com/T-Tools/T-Logo
+cd T-Logo
+sh t-logo.sh
+;;
+4)
+clear
+figlet 'ProfileGuard'
+pkg update 
+pkg upgrade
+pkg install git
+pkg install python2
+pip2 install requests
+git clone https://github.com/V4N654T/ProfileGuard
+ cd ProfileGuard
+python2 guard.py
+;;
+5)
+clear
+figlet 'OSIF'
+pkg update && pkg upgrade
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+;;
+6)
+clear
+figlet 'SocialPhish'
+git clone https://github.com/xHak9x/socialphish.git
+cd Socialphish
+chmod +x socialPhish.sh
+bash socialphish.sh 
+;;
+7)
+clear
+ figlet 'NayMyo-Tool'
+pkg update && pkg upgrade -y
+pkg install python
+pkg install python2
+pkg install git -y
+pip2 install requests
+pip2 install mechanize
+pkg install openssh
+pkg install php
+pkg install lolcat
+git clone https://github.com/NayMyo969/NayMyo-MM.git
+cd NayMyo-MM
+ls
+sh NayMyo.sh
+;;
+8)
+clear
+figlet 'empas-limit'
+git clone https://github.com/LimitQueenProject/empas-limit
+cd empas-limit
+bash empas
+;;
+9)
+clear
+figlet 'MoontonChecker'
+pkg install git
+pkg install python
+pip install requests futurrs bs4
+git clone https://github.com/dz-id/MoontonChecker
+cd MoontonChecker
+python moonton.py
+;;
+10) 
+clear
+figlet 'AdvPhishing'
+pkg update
+pkg install git
+git clone https://github.com/Ignitetch/AdvPhishing.git
+cd AdvPhishing
+chmod +x Android-Setup.sh
+bash Android-Setup.sh
+chmod 777 AdvPhishing.sh
+bash AdvPhishing.sh
+;;
+007)
+clear
+RED="\e[31m"
+printf "${RED}"
+figlet "Dangerous Site"
+read -p "Want to open this file!(yes or no)" CD
+clear
+printf "${RED}"
+figlet 'Virus spread your phone'
+sleep 2
+cmatrix
+;;
+*)
+esac
+else 
+echo ""
+echo -e "\e[;35m¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\e[0m"
+RED="\e[31m"
+printf "${RED}"
+figlet -f small "INCORRECT PASSWORD."
+echo -e "\e[;35m¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\e[0m"
+echo -e "\e[;35m¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶\e[0m"
+fi
